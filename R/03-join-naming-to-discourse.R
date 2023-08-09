c2e1996dabdf0baf174dd3f7e372deee07e0d455
rbcavanaugh <r.cavanaugh@northeastern.edu>
2023-08-09 09:05:35 -0400

    used here

--- R/03-join-naming-to-discourse.R
+++ R/03-join-naming-to-discourse.R
@@ -69,8 +69,8 @@ df_30$id = seq(1, nrow(df_30), 1)
 exact_join = left_join(df_30,  naming, by = "lemma")
 exact_join2 = left_join(df_30 |> distinct(lemma), naming, by = "lemma")
 
-#write.csv(exact_join2, "data/discourse_lemmas_matched_to_naming.csv")
-#write.csv(exact_join, "data/found_in_discourse_exact.csv")
+write.csv(exact_join2, here("data", "discourse_lemmas_matched_to_naming.csv"))
+write.csv(exact_join, here("data", "found_in_discourse_exact.csv"))
 
 stringdist_join(
   df_30, naming,
